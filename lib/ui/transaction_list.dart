6eec383e2928bde1bed4046c994b284b8d895ab8
Roberto Notario <notariorob@gmail.com>
2022-05-08 11:00:30 +0100

    use ListTile instead of custom list item

--- lib/ui/transaction_list.dart
+++ lib/ui/transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_app/models/transaction.dart';
-import 'package:expense_app/ui/transaction_list_item.dart';
+import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -13,8 +13,26 @@ class TransactionList extends StatelessWidget {
       height: 300,
       child: ListView.builder(
         itemCount: transactions.length,
-        itemBuilder: (context, index) => TransactionListItem(
-          transaction: transactions[index],
+        itemBuilder: (context, index) => Card(
+          elevation: 3,
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: FittedBox(
+                child: Text(
+                  transactions[index].amount.toStringAsFixed(2),
+                ),
+              ),
+            ),
+            title: Text(
+              transactions[index].title,
+            ),
+            subtitle: Text(
+              DateFormat.yMMMd().format(
+                transactions[index].date,
+              ),
+            ),
+          ),
         ),
       ),
     );
