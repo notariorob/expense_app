5c3d74c04ac31d5ce49261fbd8ce315225de657a
Roberto Notario <notariorob@gmail.com>
2022-05-09 09:55:15 +0100

    add label to button if list tile > 400

--- lib/ui/transaction_list_item_alt.dart
+++ lib/ui/transaction_list_item_alt.dart
@@ -11,29 +11,39 @@ class TransactionListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 30,
-          child: FittedBox(
-            child: Text(
-              transaction.amount.toStringAsFixed(2),
+    return LayoutBuilder(
+      builder: (context, constraints) => Card(
+        elevation: 3,
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            child: FittedBox(
+              child: Text(
+                transaction.amount.toStringAsFixed(2),
+              ),
             ),
           ),
-        ),
-        title: Text(
-          transaction.title,
-        ),
-        subtitle: Text(
-          DateFormat.yMMMd().format(
-            transaction.date,
+          title: Text(
+            transaction.title,
           ),
-        ),
-        trailing: IconButton(
-          icon: const Icon(Icons.delete),
-          color: Theme.of(context).errorColor,
-          onPressed: onDelete,
+          subtitle: Text(
+            DateFormat.yMMMd().format(
+              transaction.date,
+            ),
+          ),
+          trailing: constraints.maxWidth > 400
+              ? TextButton.icon(
+                  icon: const Icon(Icons.delete),
+                  style: TextButton.styleFrom(
+                      primary: Theme.of(context).errorColor),
+                  onPressed: onDelete,
+                  label: const Text('Delete'),
+                )
+              : IconButton(
+                  icon: const Icon(Icons.delete),
+                  color: Theme.of(context).errorColor,
+                  onPressed: onDelete,
+                ),
         ),
       ),
     );
